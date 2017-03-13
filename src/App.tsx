9dbccd12b143505d23d4dada195761916b2fa14b
Antonio <munoztorres.antonio@gmail.com>
2017-03-14 00:14:53 +0100

    Kick Off

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
-import './App.css';
-
-const logo = require('./logo.svg');
 
 class App extends React.Component<null, null> {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="right_col" role="main">
+            <div className="">
+                <div className="page-title">
+                    <div className="title_left">
+                        <h3>Plain Page</h3>
+                    </div>
+                </div>
+                <div className="clearfix"/>
+                <div className="row">
+                    <div className="col-md-12 col-sm-12 col-xs-12">
+                        <div className="x_panel">
+                            <div className="x_title">
+                                <h2>Plain Page</h2>
+                            </div>
+                            <div className="x_content"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
